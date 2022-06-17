6e082dd65f4a74731853e683a086f2fbb6abba94
PKcheis <philipkubaichege@gmail.com>
2022-06-18 01:50:39 +0300

    completed tool

--- prompetfun.c
+++ prompetfun.c
@@ -1,124 +1,95 @@
 #include "shell.h"
-
-int cant_open(char *file_path);
-int proc_file_commands(char *file_path, int *exe_ret);
-
 /**
- * cant_open - If the file doesn't exist or lacks proper permissions, print
- * a cant open error.
- * @file_path: Path to the supposed file.
+ * _getenv - gets the path
+ * @path_name: a pointer to the struct of data
  *
- * Return: 127.
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
  */
-
-int cant_open(char *file_path)
+char *_getenv(char *path_name)
 {
-	char *error, *hist_str;
-	int len;
+	char **environ_cursor, *env_ptr, *name_ptr;
 
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		return (127);
-
-	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
+	environ_cursor = environ;
+	while (*environ_cursor)
 	{
-		free(hist_str);
-		return (127);
+		env_ptr = *environ_cursor;
+		name_ptr = path_name;
+		while (*env_ptr == *name_ptr)
+		{
+			if (*env_ptr == '=')
+				break;
+			env_ptr++;
+			name_ptr++;
+		}
+		if ((*env_ptr == '=') && (*name_ptr == '\0'))
+			return (env_ptr + 1);
+		environ_cursor++;
 	}
+	return (NULL);
+}
+/**
+ * signal_handler - handle the process interrept signal
+ * @signo: the signal identifier
+ *
+ * Return: void
+ */
+void signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		PRINT("\n");
+		PRINT(PROMPT);
+	}
+}
+/**
+ * fill_an_array - fill an array with elements
+ * @a: the given array
+ * @el: the given element
+ * @len: the length of the array
+ *
+ * Return: pointer to filled array
+ */
+void *fill_an_array(void *a, int el, unsigned int len)
+{
+	char *p = a;
+	unsigned int i = 0;
 
-	_strcpy(error, name);
-	_strcat(error, ": ");
-	_strcat(error, hist_str);
-	_strcat(error, ": Can't open ");
-	_strcat(error, file_path);
-	_strcat(error, "\n");
-
-	free(hist_str);
-	write(STDERR_FILENO, error, len);
-	free(error);
-	return (127);
+	while (i < len)
+	{
+		*p = el;
+		p++;
+		i++;
+	}
+	return (a);
 }
 
 /**
- * proc_file_commands - Takes a file and attempts to run the commands stored
- * within.
- * @file_path: Path to the file.
- * @exe_ret: Return value of the last executed command.
+ * array_rev - reverse array
+ * @arr: the given array
+ * @len: the array length
  *
- * Return: If file couldn't be opened - 127.
- *	   If malloc fails - -1.
- *	   Otherwise the return value of the last command ran.
+ * Return: void
  */
-int proc_file_commands(char *file_path, int *exe_ret)
+void array_rev(char *arr, int len)
 {
-	ssize_t file, b_read, i;
-	unsigned int line_size = 0;
-	unsigned int old_size = 120;
-	char *line, **args, **front;
-	char buffer[120];
-	int ret;
+	int i;
+	char tmp;
 
-	hist = 0;
-	file = open(file_path, O_RDONLY);
-	if (file == -1)
+	for (i = 0; i < (len / 2); i++)
 	{
-		*exe_ret = cant_open(file_path);
-		return (*exe_ret);
+		tmp = arr[i];
+		arr[i] = arr[(len - 1) - i];
+		arr[(len - 1) - i] = tmp;
 	}
-	line = malloc(sizeof(char) * old_size);
-	if (!line)
-		return (-1);
-	do {
-		b_read = read(file, buffer, 119);
-		if (b_read == 0 && line_size == 0)
-			return (*exe_ret);
-		buffer[b_read] = '\0';
-		line_size += b_read;
-		line = _realloc(line, old_size, line_size);
-		_strcat(line, buffer);
-		old_size = line_size;
-	} while (b_read);
-	for (i = 0; line[i] == '\n'; i++)
-		line[i] = ' ';
-	for (; i < line_size; i++)
-	{
-		if (line[i] == '\n')
-		{
-			line[i] = ';';
-			for (i += 1; i < line_size && line[i] == '\n'; i++)
-				line[i] = ' ';
-		}
-	}
-	variable_replacement(&line, exe_ret);
-	handle_line(&line, line_size);
-	args = _strtok(line, " ");
-	free(line);
-	if (!args)
-		return (0);
-	if (check_args(args) != 0)
-	{
-		*exe_ret = 2;
-		free_args(args, args);
-		return (*exe_ret);
-	}
-	front = args;
-
-	for (i = 0; args[i]; i++)
-	{
-		if (_strncmp(args[i], ";", 1) == 0)
-		{
-			free(args[i]);
-			args[i] = NULL;
-			ret = call_args(args, front, exe_ret);
-			args = &args[++i];
-			i = 0;
-		}
-	}
-
-	ret = call_args(args, front, exe_ret);
-
-	free(front);
-	return (ret);
+}
+/**
+ * index_cmd - indexed command
+ * @data: a pointer to the data structure
+ *
+ * Return: void
+ */
+void index_cmd(sh_t *data)
+{
+	data->index += 1;
 }
